6fe1685e0f2466e45c6cc3eab9bd3cd16b9a159e
Igor Guba <gubaigor@gmail.com>
2019-04-28 17:15:57 -0400

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Hawaii burger');
+INSERT INTO burgers (burger_name) VALUES ('Barbecue burger	');
+INSERT INTO burgers (burger_name) VALUES ('Steak burger');
+INSERT INTO burgers (burger_name) VALUES ('Teriyaki burger');
